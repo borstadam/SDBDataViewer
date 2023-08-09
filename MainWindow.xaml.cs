9b6ae2095bad3a4260e7874ed38435634d55ca65
Adam Borst <Adam.Borst@Meridianbioscience.com>
2023-08-09 09:28:35 -0400

    changed aesthetic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,24 +25,10 @@ namespace SDB_Data_Viewer
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            // List Box example change depending on CB selection
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
